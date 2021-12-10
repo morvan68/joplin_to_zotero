164446362043ab185434e1b2c1f0355409c6eda4
morvan68 <martintowner@gmail.com>
2021-12-11 06:59:09 +0800

    rework

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # joplin_to_zotero
 small utility to move notes from joplin to a collection within zotero
+(or more generally create a collection within zotero based on a folder structure with text/md files)
+
+md version
+----------
+in joplin, run 
+    joplin export all as md
+This builds a folder tree with your exported joplin notes as md files within substructures
+
+Then the main script j_to_z.py will walk this folder tree and create same structure within zotero
+note that tags are not transferred when using md version
+
+json version
+------------
+in joplin run 
+    TBD joplin export 
+this creates a flat folder with json files with all the tags/parent structure embedded.
+import not yet implemented, but will support tags
+
---
+++ j_to_z.py
@@ -0,0 +1,92 @@
+#@+leo-ver=5-thin
+#@+node:martin.20210515201503.2: * @file ./j_to_z.py
+#@@language python
+#@@tabwidth -4
+
+#@+others
+#@+node:martin.20210605192255.2: ** Declarations (j_to_z.py)
+import os
+import sys
+
+from pyzotero import zotero
+
+#@+node:martin.20210605192255.3: ** j_to_z (j_to_z.py)
+def mdtree_to_z( sdir , api_key):
+    """
+    md option (it looses any tags)
+    before running:
+    
+    joplin export all as md
+    
+    """
+    library_id = ''
+    library_type = ''
+    zot = zotero.Zotero(library_id, library_type, api_key)
+
+    items = zot.top(limit=5)
+    # we've retrieved the latest five top-level items in our library
+    # we can print each item's item type and ID
+    if items == [] or items == None:
+        print( 'something failed')
+        exit()
+    for item in items:
+        print('Item: %s | Key: %s' % (item['data']['itemType'], item['data']['key']))
+    
+       
+    # load text items from sdir to import them
+    #this needs to be walk, to build a dict/subdict tree structure
+    loaded_dict = {}
+    for dirpath, filenames, dirnames in os.walk( sdir):
+#    for fdir in dir_folders:
+        #check if exisit subcollectiom:
+        #if not, creat subcollection from fdir
+#        loaded_items = []
+        loaded_dict[dirpath] = {}
+        for ii in filenames:
+#           read each md, save as standalonenote using api
+            if  ii.endswith('.md'):
+                with open( os.path.join( dirpath, ii), 'rt') as fp:
+                    xx = fp.readlines()
+                    print( ii, '\n------\n', xx)
+                    loaded_dict[dirpath][ii[:-4]] = xx
+    #not lets print what we've got to check
+    print( loaded_dict)
+    
+    exit()
+        
+        #recurs the dict, sending item to zotero
+        #or can we do the whole dict in one go??
+        
+        #zot.create_items( loaded_items[, parentid, last_modified])
+            #create a thing, need to poll parent to get parent_id
+
+"""
+            items (list) – one or more dicts containing item data
+            parentid (str) – A Parent item ID. This will cause the item(s) to become the child items of the given parent ID
+            last_modified (str/int) – If not None will set the value of the If-Unmodified-Since-Version header.
+"""
+
+#@+node:martin.20210605234949.1: ** j_to_z_json
+def j_to_z_json( sdir, api_key):
+    ...
+    """
+    tag supporting option
+    #jopline export as json
+    
+    reald local folder
+                read jsons
+                build data structure in memory as json/dicts
+                connect to api
+                write all the tags to collection
+                write subcollectiosn, itesms, tags
+    """
+
+#@+node:martin.20210605192456.1: ** main
+if __name__ == '__main__':
+    sourcedir = sys.argv[1]
+    api_key = sys.argv[2]
+    mdtree_to_z( sourcedir, api_key)
+
+    
+#@-others
+#@-leo
