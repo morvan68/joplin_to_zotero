397008114686eed390a52a9d3a15a9d98c25c41e
morvan68 <martintowner@gmail.com>
2023-03-13 12:36:48 +0000

    ongoing

--- README.md
+++ README.md
@@ -1,20 +1,36 @@
 # joplin_to_zotero
-small utility to move notes from joplin to a collection within zotero
-(or more generally create a collection within zotero based on a folder structure with text/md files)
+Small utility to create a collection within online zotero based on
+a local folder structure+text files, or a flat JSON folder.
 
-md version
-----------
+This can be used to to move notes from joplin to a collection within zotero,
+by using joplin to export its data as a md or JSON, then using this script
+to import into zotero.
+
+requirements:
+python3
+pyzotero
+you need an online zotero desitnation, and api_key, and need to know colelctio type
+and destination sub-collection
+
+## md version
 in joplin, run 
     joplin export all as md
 This builds a folder tree with your exported joplin notes as md files within substructures
 
-Then the main script j_to_z.py will walk this folder tree and create same structure within zotero
-note that tags are not transferred when using md version
+Then the main script j_to_z.py will walk this folder tree and create same structure within zotero.
 
-json version
-------------
+Note that tags are not transferred when using md version
+
+python3 j_to_z.py /path/to/base_folder API_KEY
+
+## json version
 in joplin run 
-    TBD joplin export 
+    TBD joplin export
+
 this creates a flat folder with json files with all the tags/parent structure embedded.
-import not yet implemented, but will support tags
+j_to_z will then support tag import into zotero.
+
+not yet implemented...
+
+python3 j_to_z.py /path/to/base_folder API_KEY JSON
 
--- j_to_z.py
+++ j_to_z.py
@@ -8,56 +8,12 @@
 import os
 import sys
 
+from functools import reduce
+
 from pyzotero import zotero
 
-#@+node:martin.20210605192255.3: ** j_to_z (j_to_z.py)
-def mdtree_to_z( sdir , api_key):
-    """
-    md option (it looses any tags)
-    before running:
-    
-    joplin export all as md
-    
-    """
-    # load text items from sdir to import them
-    #this needs to be walk, to build a dict/subdict tree structure
-    loaded_dict = {}
-    
-    loaded_dict = dir_to_dict( sdir)    
-
-    print( loaded_dict)
-
-    library_id = ''
-    library_type = ''
-    zot = zotero.Zotero(library_id, library_type, api_key)
-
-    items = zot.top(limit=5)
-    # we've retrieved the latest five top-level items in our library
-    # we can print each item's item type and ID
-    if items == [] or items == None:
-        print( 'something failed')
-        exit()
-    for item in items:
-        print('Item: %s | Key: %s' % (item['data']['itemType'], item['data']['key']))
-    
-      
-    
-    exit()
-        
-        #recurs the dict, sending item to zotero
-        #or can we do the whole dict in one go??
-        
-        #zot.create_items( loaded_items[, parentid, last_modified])
-            #create a thing, need to poll parent to get parent_id
-
-"""
-            items (list) – one or more dicts containing item data
-            parentid (str) – A Parent item ID. This will cause the item(s) to become the child items of the given parent ID
-            last_modified (str/int) – If not None will set the value of the If-Unmodified-Since-Version header.
-"""
-
-#@+node:martin.20211211160450.1: ** dir_to_dict
-def dir_to_dict (rootdir):
+#@+node:martin.20211211160450.1: ** mdtree_to_dict
+def mdtree_to_dict (rootdir):
     """
     Creates a nested dictionary that represents the folder structure of rootdir
     """
@@ -74,6 +30,10 @@ def dir_to_dict (rootdir):
 
         parent[ folders[-1]] = subdir
     
+    ###
+    test_print( dir_dict)
+    ###
+    
     return dir_dict
 
 """    
@@ -88,9 +48,57 @@ def dir_to_dict (rootdir):
 """
 
 
+#@+node:martin.20230313081000.1: ** test_print
+def test_print( mydict):
+    """used in testing to check the dict is correctly constructed based
+    on the importing"""
+
+    import pprint      
+    pprint.pprint( mydict)
+        
+#    for i in mydict:
+#        print( mydict[i])
+#@+node:martin.20210605192255.3: ** dict_to_z
+def dict_to_z( sdict , api_key):
+    """
+    write dict to zotero collection
+    currently does not support tags
+    """
+    library_id = ''
+    library_type = ''
+    zot = zotero.Zotero(library_id, library_type, api_key)
+
+
+    if items == [] or items == None:
+        print( 'something failed')
+        exit()
+    for item in items:
+        print('Item: %s | Key: %s' % (item['data']['itemType'], item['data']['key']))
+        
+        if item.endswith('.md'):
+            #its a file
+            #load the file
+            title = item[:-3]
+        else:
+            #its a sub dict
+            ...
+            
+            #zot.create_items( loaded_items[, parentid, last_modified])
+            #create a thing, need to poll parent to get parent_id
+
+"""
+    items (list) – one or more dicts containing item data
+    parentid (str) – A Parent item ID. This will cause the item(s) to become the child items of the given parent ID
+    last_modified (str/int) – If not None will set the value of the If-Unmodified-Since-Version header.
+
+    items = zot.top(limit=5)
+    # we've retrieved the latest five top-level items in our library
+    # we can print each item's item type and ID
+
+"""
+
 #@+node:martin.20210605234949.1: ** j_to_z_json
 def j_to_z_json( sdir, api_key):
-    ...
     """
     tag supporting option
     #jopline export as json
@@ -102,13 +110,18 @@ def j_to_z_json( sdir, api_key):
                 write all the tags to collection
                 write subcollectiosn, itesms, tags
     """
-
+    
 #@+node:martin.20210605192456.1: ** main
 if __name__ == '__main__':
     sourcedir = sys.argv[1]
-    api_key = sys.argv[2]
-    mdtree_to_z( sourcedir, api_key)
-
-    
+    if len(sys.argv) > 2:
+        api_key = sys.argv[2]
+    else:
+        api_key = 'dummy'
+    if len(sys.argv) > 3 and sys.argv[3] == 'JSON':
+        md_dict = j_to_z_json( sourcedir, api_key)
+    else:
+        md_dict = mdtree_to_dict( sourcedir)
+        #dict_to_z( md_dict, api_key)
 #@-others
 #@-leo
